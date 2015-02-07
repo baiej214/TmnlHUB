875b3e3399725a2347bbd8e6de27eb80f5bf15df
baiej214@Gmail.com <baiej214@Gmail.com>
2015-02-07 17:03:57 +0800

    调整升级界面

--- admin/web/js/deviceUpdate.js
+++ admin/web/js/deviceUpdate.js
@@ -1,91 +1,118 @@
 Ext.define('js.deviceUpdate', {
     formpanel: function () {
-        var _self = this;
+        var _self = this,
+            ipregex = /^(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$/;//:\d{0,5}$
         return Ext.create('Ext.form.Panel', {
-            id: 'deviceUpdateFrompanel', region: 'north', height: 300,
-            fieldDefaults: {
-                labelWidth: 60
-            },
+            id: 'deviceUpdateFrompanel', region: 'north', height: 300, fieldDefaults: {labelWidth: 60},
             items: [
-                {xtype: 'filefield', name: 'UPG', fieldLabel: '升级文件', allowBlank: false, width: 500, buttonText: '浏览'},
                 {
-                    xtype: 'fieldcontainer', fieldLabel: '设备范围', layout: 'hbox',
+                    layout: 'column', padding: '0 0 10 0',
                     items: [
                         {
-                            xtype: 'textfield', name: 'range', id: 'deviceUpdateRange',
-                            readOnly: true, padding: '0 5 0 0',
-                            listeners: {
-                                change: function (field, newValue, oldValue) {
-                                    var arr = newValue.split(','), str = '';
-                                    if (arr.length > 5) {
-                                        str = arr.slice(0, 4).concat('...').join(', ');
-                                        field.setValue(str);
-                                    }
-                                    //Ext.getCmp('deviceUpdateRangeRaw').setValue(newValue);
-                                }
-                            }
+                            xtype: 'radiogroup',
+                            fieldLabel: '升级方式',
+                            width: 200,
+                            columns: 2,
+                            vertical: true,
+                            items: [
+                                {boxLabel: '09', name: 'selectType', inputValue: '09'},
+                                {boxLabel: '13', name: 'selectType', inputValue: '13', checked: true}
+                            ]
                         },
                         {
-                            xtype: 'button', text: '选择',
-                            handler: function () {
-                                //var range = Ext.getCmp('deviceUpdateRange');
-                                //range.setValue('1,2,3,4,5,6,7,8,9,0');
-                                var selector = Ext.create('js.deviceSelector');
-                                Ext.create('Ext.window.Window', {
-                                    width: 500, height: 300, layotu: 'border', modal: true,
-                                    items: [{
-                                        id: 'selectType',
-                                        xtype: 'radiogroup', region: 'north',
-                                        fieldLabel: 'Two Columns',
-                                        columns: 2,
-                                        vertical: true,
-                                        items: [
-                                            {boxLabel: '全部', name: 'selectType', inputValue: '1', checked: true},
-                                            {boxLabel: '选择', name: 'selectType', inputValue: '2'}
-                                        ]
-                                    }, {
-                                        region: 'center', items: selector
-                                    }],
-                                    buttons: [{
-                                        text: 'OK',
-                                        handler: function () {
-                                            var selectType = Ext.getCmp('selectType').getValue().selectType,
-                                                deviceRange = Ext.getCmp('deviceRange'),
-                                                deviceUpdateRange = Ext.getCmp('deviceUpdateRange');
-                                            if (selectType == 1) {
-                                                deviceRange.setValue('all');
-                                                deviceUpdateRange.setValue('全部');
-                                            } else {
-                                                deviceRange.setValue(selector.getValue());
-                                                deviceUpdateRange.setValue(selector.getValue());
+                            xtype: 'fieldcontainer', fieldLabel: '设备范围', layout: 'hbox',
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    name: 'range',
+                                    id: 'deviceUpdateRange',
+                                    readOnly: true,
+                                    padding: '0 3 0 0',
+                                    listeners: {
+                                        change: function (field, newValue, oldValue) {
+                                            var arr = newValue.split(','), str = '';
+                                            if (arr.length > 5) {
+                                                str = arr.slice(0, 4).concat('...').join(', ');
+                                                field.setValue(str);
                                             }
-                                            this.up('window').close();
+                                            //Ext.getCmp('deviceUpdateRangeRaw').setValue(newValue);
                                         }
-                                    }]
-                                }).show();
-                            }
-                        }]
+                                    }
+                                },
+                                {
+                                    xtype: 'button', text: '选择',
+                                    handler: function () {
+                                        //var range = Ext.getCmp('deviceUpdateRange');
+                                        //range.setValue('1,2,3,4,5,6,7,8,9,0');
+                                        var selector = Ext.create('js.deviceSelector');
+                                        Ext.create('Ext.window.Window', {
+                                            width: 500, height: 300, layotu: 'border', modal: true,
+                                            items: [{
+                                                id: 'selectType',
+                                                xtype: 'radiogroup', region: 'north',
+                                                fieldLabel: 'Two Columns',
+                                                columns: 2,
+                                                vertical: true,
+                                                items: [
+                                                    {
+                                                        boxLabel: '全部',
+                                                        name: 'selectType',
+                                                        inputValue: '1',
+                                                        checked: true
+                                                    },
+                                                    {boxLabel: '选择', name: 'selectType', inputValue: '2'}
+                                                ]
+                                            }, {
+                                                region: 'center', items: selector
+                                            }],
+                                            buttons: [{
+                                                text: 'OK',
+                                                handler: function () {
+                                                    var selectType = Ext.getCmp('selectType').getValue().selectType,
+                                                        deviceRange = Ext.getCmp('deviceRange'),
+                                                        deviceUpdateRange = Ext.getCmp('deviceUpdateRange');
+                                                    if (selectType == 1) {
+                                                        deviceRange.setValue('all');
+                                                        deviceUpdateRange.setValue('全部');
+                                                    } else {
+                                                        deviceRange.setValue(selector.getValue());
+                                                        deviceUpdateRange.setValue(selector.getValue());
+                                                    }
+                                                    this.up('window').close();
+                                                }
+                                            }]
+                                        }).show();
+                                    }
+                                }]
+                        },
+                        {
+                            xtype: 'filefield', padding: '0 0 0 10',
+                            name: 'UPG',
+                            fieldLabel: '升级文件',
+                            allowBlank: false,
+                            width: 300,
+                            buttonText: '浏览'
+                        }
+                    ]
                 },
                 {
-                    xtype: 'datefield', name: 'currentReleaseDate', id: 'currentReleaseDate', fieldLabel: '发布日期',
-                    allowBlank: false, editable: false, padding: '0 0 10 0', maxValue: new Date(), format: 'Y-m-d'
-                },
-                {
-                    xtype: 'fieldset', border: false, padding: 0,
-                    checkboxToggle: true,
-                    title: '重启后配置通讯参数',
+                    layout: 'column',
                     items: [
+                    //    {
+                    //    xtype: 'fieldset', border: false,
+                    //    checkboxToggle: true,
+                    //    title: '重启后配置通讯参数',
+                    //    items: []
+                    //},
+                        {
+                            xtype: 'checkbox', boxLabel: '重启后配置通讯参数', chekced: true,
+                        },
                         {
                             xtype: 'fieldcontainer',
                             fieldLabel: '主IP',
                             layout: 'hbox',
                             items: [
-                                {
-                                    xtype: 'textfield',
-                                    name: 'mainip',
-                                    emptyText: '0.0.0.0',
-                                    regex: /^(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$///:\d{0,5}$
-                                },
+                                {xtype: 'textfield', name: 'mainip', emptyText: '0.0.0.0', regex: ipregex},
                                 {
                                     name: 'mainport',
                                     xtype: 'numberfield',
@@ -100,33 +127,7 @@ Ext.define('js.deviceUpdate', {
                                 }
                             ]
                         },
-                        {
-                            xtype: 'fieldcontainer',
-                            fieldLabel: '备IP',
-                            layout: 'hbox',
-                            items: [
-                                {
-                                    xtype: 'textfield',
-                                    name: 'backip',
-                                    emptyText: '0.0.0.0',
-                                    regex: /^(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$///:\d{0,5}$
-                                },
-                                {
-                                    name: 'backport',
-                                    xtype: 'numberfield',
-                                    mouseWheelEnabled: false,
-                                    allowDecimals: false,
-                                    hideTrigger: true,
-                                    emptyText: '端口',
-                                    maxValue: 65535,
-                                    minValue: 0,
-                                    padding: '0 5',
-                                    width: 50
-                                }
-                            ]
-                        },
-                        {xtype: 'textfield', name: 'apn', fieldLabel: 'APN'}
-                    ]
+                        {xtype: 'textfield', name: 'apn', fieldLabel: 'APN'}]
                 },
                 {xtype: 'hidden', name: 'deviceRange', id: 'deviceRange'},
                 {
