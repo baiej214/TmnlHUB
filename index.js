3acf82076fcf95b9a5de0dee902b8cfe5e4e0389
baiej214@Gmail.com <baiej214@Gmail.com>
2015-02-08 23:39:06 +0800

    添加捕获未知错误

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 var tmnl_server = require('./tmnl/tmnl_server'),
     web_server = require('./web/web_server'),
     broadcast = require('./web/broadcast'),
-    admin_server = require('./admin/admin_server');
+    admin_server = require('./admin/admin_server'),
+    cError = require('./error').Error;
 
 tmnl_server.start();
 web_server.start();
 broadcast.start();
 admin_server.start();
+
+process.on('uncaughtException', function (err) {
+    console.log('Caught exception: ' + cError(err));
+});
